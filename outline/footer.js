b17cb346816c9e58fb9b871be52280d701451875
RongbinZhuang <zhuangrb2017@gmail.com>
2017-07-19 00:40:23 +0800

    naive

---
+++ outline/footer.js
@@ -0,0 +1,8 @@
+var footer=document.createElement("footer");
+var btn1=document.createElement("button");
+var btn1Text=document.createTextNode("这是新段落。wefwef");
+btn1.appendChild(btn1Text);
+footer.appendChild(btn1);
+
+
+document.body.appendChild(footer);
---
+++ outline/header.js
@@ -0,0 +1,10 @@
+var header=document.createElement("header");
+var btn1=document.createElement("button");
+var btn1Text=document.createTextNode("Blog");
+var headDiv="wefew";
+header.appendChild()
+btn1.className="headButton";
+btn1.appendChild(btn1Text);
+header.appendChild(btn1);
+
+document.body.appendChild(header);
