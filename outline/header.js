3272a7dd87da4c9f9e1b0581b5a4f40351772af4
RongbinZhuang <zhuangrb2017@gmail.com>
2017-07-19 16:25:50 +0800

    naive

--- outline/header.js
+++ outline/header.js
@@ -1,10 +1,33 @@
-var header=document.createElement("header");
-var btn1=document.createElement("button");
-var btn1Text=document.createTextNode("Blog");
-var headDiv="wefew";
-header.appendChild()
-btn1.className="headButton";
-btn1.appendChild(btn1Text);
-header.appendChild(btn1);
+// variable define
+  // array define
+  var headBtn=new Array([]);
+  var headBtnText=new Array("Profile","Blog","Project","Connect");
+  var headDiv=new Array([]);
+
+  // element define
+  var header=document.createElement("header");
+  var Title=document.createTextNode("Homepage of Zhuang");
+// header.appendChild()
+
+headDiv[0]=document.createElement("headDiv");
+headDiv[1]=document.createElement("headDiv");
+
+
+headDiv[0].appendChild(Title);
+
+for (var idx in headBtnText){
+  headBtn[idx]=document.createElement("button");
+  headBtn[idx].className="headButton";
+  headBtn[idx].appendChild(document.createTextNode(headBtnText[idx]));
+  headDiv[1].appendChild(headBtn[idx]);
+}
+
+headDiv[0].style="float:left;";
+headDiv[1].style="float:right;";
+
+// window.alert("check");
+// link the elemen
+header.appendChild(headDiv[0]);
+header.appendChild(headDiv[1]);
 
 document.body.appendChild(header);
